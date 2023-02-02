b1bcc4ec7db02cdc11ab8ddebb28b633174445bb
Rosa Turner <rmturner1220@gmail.com>
2023-02-01 19:46:12 -0500

    initial commit

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.jpg";
+import product9 from "./assets/9.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Xbox Wireless Controller",
+    price: 59.12,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Smartwatch",
+    price: 150,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Bluetooth Speaker",
+    price: 55,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "MacBook Air",
+    price: 999,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Headphones",
+    price: 39.05,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Nikon D750 Camera",
+    price: 1295,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Apple Ipad ",
+    price: 999,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Samsung USB flash drive",
+    price: 49,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Router",
+    price: 53,
+    productImage: product9,
+  },
+];
